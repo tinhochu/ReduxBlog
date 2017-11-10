2347c1983be3c09f6fd8dc8f305614f9bcb1e357
Tin Chu <>
2017-11-10 12:32:31 -0600

    Posts New Route and Component

--- src/components/post_index.js
+++ src/components/post_index.js
@@ -1,13 +1,44 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from "../actions/index";
+import { Link } from 'react-router-dom';
 
-class PostIndex extends Component {
+
+class PostsIndex extends Component {
+
+	componentDidMount(){
+		this.props.fetchPosts();
+	}
+	
+	renderPosts(){
+		return _.map(this.props.posts, post => {
+			return(
+				<li className="list-group-item" key={post.id}>{post.title}</li>
+			);
+		});
+	}
+	
 	render() {
+		// console.log(this.props.posts);
 		return (
 			<div>
-				Posts Index
+				<div className="text-xs-right">
+					<Link className="btn btn-primary" to="/posts/new">
+						Add a Post
+					</Link>
+				</div>
+				<h3>Posts</h3>
+				<ul className="list-group">
+					{ this.renderPosts() }
+				</ul>
 			</div>
 		);
 	}
 }
 
-export default PostIndex;
+function mapStateToProps(state){
+	return { posts: state.posts };
+}
+
+export default connect( mapStateToProps, { fetchPosts } )(PostsIndex);
---
+++ src/components/post_new.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class PostNew extends Component {
+	render() {
+		return (
+			<div>
+				<h3>New Post</h3>
+			</div>
+		);
+	}
+}
+
+export default PostNew;
